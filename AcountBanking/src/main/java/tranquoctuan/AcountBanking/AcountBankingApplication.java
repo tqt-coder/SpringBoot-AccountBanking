664c723b8340287e90e265c7cabcefc81e6a45e1
tranquoctuan2001 <76949884+tranquoctuan2001@users.noreply.github.com>
2022-01-16 00:46:28 +0700

    initialize project

---
+++ AcountBanking/src/main/java/tranquoctuan/AcountBanking/AcountBankingApplication.java
@@ -0,0 +1,13 @@
+package tranquoctuan.AcountBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcountBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcountBankingApplication.class, args);
+	}
+
+}
---
+++ AcountBanking/src/test/java/tranquoctuan/AcountBanking/AcountBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package tranquoctuan.AcountBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcountBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
